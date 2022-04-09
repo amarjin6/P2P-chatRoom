fd6bb8f582674be2a2c78779a29892880e239afd
Alexander Marjin <amarjin6@gmail.com>
2022-04-09 23:18:28 +0300

    Create Connect function

--- chat.py
+++ chat.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 UDP_SIZE = 65535  # UDP protocol maximus
 
@@ -17,6 +18,10 @@ python chat.py 192.168.0.1 John
 '''
 
 
+def greeting():
+    print('Welcome to chat!')
+
+
 def usage():
     print(USAGE)
 
@@ -24,20 +29,42 @@ def usage():
 def listen(host: str = DEFAULT_IP, port: int = DEFAULT_PORT):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Create socket object on UDP connection
     s.bind((host, port))
-
-    members = []
+    members = {}
     while True:
         msg, addr = s.recvfrom(UDP_SIZE)
-
-        if addr not in members:
-            members.append(addr)
-            # Show message that new Client joined chat
 
         if not msg:
             continue
 
         msg_text = msg.decode('utf-8')
+        if msg_text[:6] == '__join' and addr not in members:
+            members[addr] = msg[6:].decode()
+            for addr in members.keys():
+                s.sendto(f'{msg[6:].decode()} joined chat'.encode('utf-8'), addr)
+            continue
+
+
+def connect():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    IP = input('Enter IP:')
+    port = int(input('Enter port:'))
+    name = input('Enter name:')
+    s.bind((IP, port))
+    sendto = (DEFAULT_IP, DEFAULT_PORT)
+    s.sendto(f'__join{name}'.encode('utf-8'), sendto)
+    while True:
+        msg, addr = s.recvfrom(UDP_SIZE)
+
+        if not msg:
+            continue
+
+        msg_text = msg.decode('utf-8')
+        print(msg_text)
 
 
 if __name__ == '__main__':
-    ...
+    greeting()
+    t1 = threading.Thread(target=listen)
+    t2 = threading.Thread(target=connect)
+    t1.start()
+    t2.start()
